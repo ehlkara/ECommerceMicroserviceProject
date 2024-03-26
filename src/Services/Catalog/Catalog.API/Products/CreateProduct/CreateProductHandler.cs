b8befe1ba5047b85b5f2b0fba435e8bca8221d32
ehlkara <ehlullah.fb@gmail.com>
2024-03-26 09:57:02 +0300

    Implemented CQRS Abstractions ICommand into vertical slice

--- src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -1,16 +1,29 @@
-﻿using MediatR;
+﻿using BuildingBlocks.CQRS;
+using Catalog.API.Models;
 
 namespace Catalog.API.Products.CreateProduct;
 
 public record CreateProductCommand(string Name, List<string> Category, string Description, string ImageFile, decimal Price)
-    : IRequest<CreatProductResult>;
-public record CreatProductResult(Guid Id);
+    : ICommand<CreateProductResult>;
+public record CreateProductResult(Guid Id);
 
-internal class CreateProductCommandHandler : IRequestHandler<CreateProductCommand, CreatProductResult>
+internal class CreateProductCommandHandler : ICommandHandler<CreateProductCommand, CreateProductResult>
 {
-    public Task<CreatProductResult> Handle(CreateProductCommand request, CancellationToken cancellationToken)
+    public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
     {
-        // Business logic Created Product
-        throw new NotImplementedException();
+        //create Product entity from command object
+        //save to database
+        //return CreateProductResult result
+
+        var product = new Product
+        {
+            Name = command.Name,
+            Category = command.Category,
+            Description = command.Description,
+            ImageFile = command.ImageFile,
+            Price = command.Price,
+        };
+
+        return new CreateProductResult(Guid.NewGuid());
     }
 }
