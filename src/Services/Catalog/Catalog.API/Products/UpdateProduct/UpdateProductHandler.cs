74b6b28f64f1231e9d71cdf165bc50c2b35bf030
ehlkara <ehlullah.fb@gmail.com>
2024-05-16 10:18:04 +0300

    added validation delete and update commandHandler

--- src/Services/Catalog/Catalog.API/Products/UpdateProduct/UpdateProductHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/UpdateProduct/UpdateProductHandler.cs
@@ -5,6 +5,16 @@ public record UpdateProductCommand(Guid Id,string Name,List<string> Category, st
 
 public record UpdateProductResult(bool IsSuccess);
 
+public class UpdateProductCommandValidator : AbstractValidator<UpdateProductCommand>
+{
+    public UpdateProductCommandValidator()
+    {
+        RuleFor(x => x.Id).NotEmpty().WithMessage("PRoduct ID is required!");
+        RuleFor(x => x.Name).NotEmpty().WithMessage("Name is required!").Length(2, 150).WithMessage("Name must be between 2 and 150 characters");
+        RuleFor(x => x.Price).GreaterThan(0).WithMessage("Price must be greater than 0!");
+    }
+}
+
 internal class UpdateProductCommandHandler(IDocumentSession session, ILogger<UpdateProductCommandHandler> logger) : ICommandHandler<UpdateProductCommand, UpdateProductResult>
 {
     public async Task<UpdateProductResult> Handle(UpdateProductCommand command, CancellationToken cancellationToken)
