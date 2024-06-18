f88c7a2bd086d50d7cdb1662b1d76d9e11c97a1a
ehlkara <ehlullah.fb@gmail.com>
2024-06-18 10:58:08 +0300

    develop GetBasketHandler

--- src/Services/Basket/Basket.API/Basket/GetBasket/GetBasketHandler.cs
+++ src/Services/Basket/Basket.API/Basket/GetBasket/GetBasketHandler.cs
@@ -1,6 +1,15 @@
-﻿namespace Basket.API.Basket.GetBasket
+﻿namespace Basket.API.Basket.GetBasket;
+
+public record GetBasketQuery(string UserName) : IQuery<GetBasketResult>;
+public record GetBasketResult(ShoppingCart Cart);
+
+public class GetBasketQueryHandler : IQueryHandler<GetBasketQuery, GetBasketResult>
 {
-    public class GetBasketHandler
+    public async Task<GetBasketResult> Handle(GetBasketQuery query, CancellationToken cancellationToken)
     {
+        //TODO: Implement the logic to get the basket from the database
+        //var basket = await _repository.GetBasket(query.UserName);
+
+        return new GetBasketResult(new ShoppingCart("swn"));
     }
 }
