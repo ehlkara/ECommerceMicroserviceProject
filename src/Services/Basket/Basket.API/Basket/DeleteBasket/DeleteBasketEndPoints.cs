174d369ff69339ad0d484220fe5853c77a7de4bd
ehlkara <ehlullah.fb@gmail.com>
2024-06-19 11:10:52 +0300

    Develop Delete basket Endpoint

--- src/Services/Basket/Basket.API/Basket/DeleteBasket/DeleteBasketEndPoints.cs
+++ src/Services/Basket/Basket.API/Basket/DeleteBasket/DeleteBasketEndPoints.cs
@@ -1,6 +1,24 @@
-﻿namespace Basket.API.Basket.DeleteBasket
+﻿namespace Basket.API.Basket.DeleteBasket;
+
+//public record DeleteBasketRequest(string UserName);
+public record DeleteBasketResponse(bool IsSuccess);
+
+public class DeleteBasketEndPoints : ICarterModule
 {
-    public class DeleteBasketEndPoints
+    public void AddRoutes(IEndpointRouteBuilder app)
     {
+        app.MapDelete("/basket/{userName}", async (string userName, ISender sender) =>
+        {
+            var result = await sender.Send(new DeleteBasketCommand(userName));
+
+            var response = result.Adapt<DeleteBasketResponse>();
+
+            return Results.Ok(response);
+        })
+        .WithName("DeleteBasketByUserName")
+        .Produces<DeleteBasketResponse>(StatusCodes.Status200OK)
+        .ProducesProblem(StatusCodes.Status404NotFound)
+        .WithSummary("Delete a basket by user name")
+        .WithDescription("Delete a basket by user name");
     }
 }
