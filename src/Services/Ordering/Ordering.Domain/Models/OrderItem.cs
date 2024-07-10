e273d11e0736d97b4320a167274a3bfd0b4c17ff
ehlkara <ehlullah.fb@gmail.com>
2024-07-10 09:01:08 +0300

    develop modelling with ddd

---
+++ src/Services/Ordering/Ordering.Domain/Models/OrderItem.cs
@@ -0,0 +1,17 @@
+﻿namespace Ordering.Domain.Models;
+
+public class OrderItem : Entity<Guid>
+{
+    internal OrderItem(Guid productId, Guid orderId, decimal price, int quantity)
+    {
+        ProductId = productId;
+        OrderId = orderId;
+        Price = price;
+        Quantity = quantity;
+    }
+
+    public Guid ProductId { get; private set; } = default!;
+    public Guid OrderId { get; private set; } = default!;
+    public decimal Price { get; private set; }
+    public int Quantity { get; private set; }
+}
