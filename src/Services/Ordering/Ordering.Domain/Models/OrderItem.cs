71a2799087ab578f0e126d98dae09c83a2bd2480
ehlkara <ehlullah.fb@gmail.com>
2024-07-27 10:15:13 +0300

    develop strongly IDs models

--- src/Services/Ordering/Ordering.Domain/Models/OrderItem.cs
+++ src/Services/Ordering/Ordering.Domain/Models/OrderItem.cs
@@ -1,17 +1,18 @@
 ﻿namespace Ordering.Domain.Models;
 
-public class OrderItem : Entity<Guid>
+public class OrderItem : Entity<OrderItemId>
 {
     internal OrderItem(ProductId productId, OrderId orderId, decimal price, int quantity)
     {
+        Id = OrderItemId.Of(Guid.NewGuid());
         ProductId = productId;
         OrderId = orderId;
         Price = price;
         Quantity = quantity;
     }
 
-    public Guid ProductId { get; private set; } = default!;
-    public Guid OrderId { get; private set; } = default!;
+    public ProductId ProductId { get; private set; } = default!;
+    public OrderId OrderId { get; private set; } = default!;
     public decimal Price { get; private set; }
     public int Quantity { get; private set; }
 }
