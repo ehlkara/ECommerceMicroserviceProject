05a1086e09564e57251a2fc7973a100c0ccd3699
ehlkara <ehlullah.fb@gmail.com>
2024-07-27 10:58:29 +0300

    develop rich model order entity

--- src/Services/Ordering/Ordering.Domain/Models/OrderItem.cs
+++ src/Services/Ordering/Ordering.Domain/Models/OrderItem.cs
@@ -2,17 +2,17 @@
 
 public class OrderItem : Entity<OrderItemId>
 {
-    internal OrderItem(ProductId productId, OrderId orderId, decimal price, int quantity)
+    internal OrderItem(OrderId orderId, ProductId productId, decimal price, int quantity)
     {
         Id = OrderItemId.Of(Guid.NewGuid());
-        ProductId = productId;
         OrderId = orderId;
+        ProductId = productId;
         Price = price;
         Quantity = quantity;
     }
 
-    public ProductId ProductId { get; private set; } = default!;
     public OrderId OrderId { get; private set; } = default!;
+    public ProductId ProductId { get; private set; } = default!;
     public decimal Price { get; private set; }
     public int Quantity { get; private set; }
 }
