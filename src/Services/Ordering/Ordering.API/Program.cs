12c91fea77d42d4ff5d6318f78e062f8cd2a4649
ehlkara <ehlullah.fb@gmail.com>
2024-07-29 14:15:04 +0300

    added globalusing

--- src/Services/Ordering/Ordering.API/Program.cs
+++ src/Services/Ordering/Ordering.API/Program.cs
@@ -5,7 +5,6 @@ using Ordering.Infrastructure;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services
     .AddApplicationServices()
     .AddInfrastructureServices(builder.Configuration)
--- src/Services/Ordering/Ordering.Infrastructure/DependencyInjection.cs
+++ src/Services/Ordering/Ordering.Infrastructure/DependencyInjection.cs
@@ -10,7 +10,7 @@ public static class DependencyInjection
         var connectionString = configuration.GetConnectionString("Database");
 
         // Add services to the container.
-        //services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(connectionString));
+        services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(connectionString));
 
         //services.AddScoped<IApplicationDbContext, ApplicationDbContext>();
 
