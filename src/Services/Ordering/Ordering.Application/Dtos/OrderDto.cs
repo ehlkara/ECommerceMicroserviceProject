227ea337a949a86d385a32e554100a045f7b68a8
Ehlullah Karakurt <ehlullah.fb@gmail.com>
2024-08-03 17:25:54 +0300

    craeted Order Commands

---
+++ src/Services/Ordering/Ordering.Application/Dtos/OrderDto.cs
@@ -0,0 +1,13 @@
+﻿using Ordering.Domain.Enums;
+
+namespace Ordering.Application.Dtos;
+
+public record OrderDto(
+    Guid Id, 
+    Guid CustomerId, 
+    string OrderName, 
+    AddressDto ShipppingAddress, 
+    AddressDto BillingAddress, 
+    PaymentDto payment,
+    OrderStatus Status, 
+    List<OrderItemDto> OrderItems);
