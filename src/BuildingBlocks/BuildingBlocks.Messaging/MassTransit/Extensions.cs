b229184b334d4719d57a424f978b1b8b1a6ebef8
Ehlullah Karakurt <ehlullah.fb@gmail.com>
2024-08-10 15:32:11 +0300

    added BasketCheckout event and masstransit library

---
+++ src/BuildingBlocks/BuildingBlocks.Messaging/MassTransit/Extensions.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace BuildingBlocks.Messaging.MassTransit;
+
+public static class Extensions
+{
+    public static IServiceCollection AddMessageBroker(this IServiceCollection services, IConfiguration configuration) 
+    {
+        return services;
+    }
+}
